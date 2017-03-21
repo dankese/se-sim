7efba09f635efbf6eee7d604607320dee36395b5
dankese <segpcj@tutanota.com>
2017-03-21 18:41:40 -0400

    Update Trader.java

--- Trader.java
+++ Trader.java
@@ -1,52 +1,75 @@
 
-public class Trader implements Comparable<Trader>{
+public class Trader implements Comparable<Trader> {
+    private Brokerage brokerage;
+    private String name;
+    private String pswd;
 
-	@Override
-	public int compareTo(Trader arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-//
-//	public Trader(Brokerage brokerage,
-//            java.lang.String name,
-//            java.lang.String pswd)
-//Constructs a new trader, affiliated with a given brockerage, with a given screen name and password.
-//
-//public java.lang.String getName()
-//Returns the screen name for this trader.
-//
-//public java.lang.String getPassword()
-//Returns the password for this trader.
-//
-//public int compareTo(Trader other)
-//Compares this trader to another by comparing their screen names case blind.
-//
-//public boolean equals(java.lang.Object other)
-//Indicates whether some other trader is "equal to" this one, based on comparing their screen names 
-//case blind. This method will throw a ClassCastException if other is not an instance of Trader.
-//
-//public void openWindow()
-//Creates a new TraderWindow for this trader and saves a reference to it in myWindow. 
-//Removes and displays all the messages, if any, from this trader's mailbox by 
-//calling myWindow.show(msg) for each message.
-//
-//public boolean hasMessages()
-//Returns true if this trader has any messages in its mailbox.
-//
-//public void receiveMessage(java.lang.String msg)
-//Adds msg to this trader's mailbox and displays all messages. If this trader is logged in 
-//(myWindow is not null) removes and shows all the messages in the mailbox by calling 
-//myWindow.showMessage(msg) for each msg in the mailbox.
-//
-//public void getQuote(java.lang.String symbol)
-//Requests a quote for a given stock symbol from the brokerage by calling brokerage's getQuote.
-//
-//public void placeOrder(TradeOrder order)
-//Places a given order with the brokerage by calling brokerage's placeOrder.
-//
-//public void quit()
-//Logs out this trader. Calls brokerage's logout for this trader. Sets myWindow to null 
-//(this method is called from a TraderWindow's window listener when the "close window" button is clicked).
-		
-		
+    @Override
+    public int compareTo(Trader other) {
+        return this.getName().toLowerCase().compareTo(other.getName().toLowerCase());
+    }
+
+    //Constructs a new trader, affiliated with a given brockerage, with a given screen name and password.
+
+    public Trader(Brokerage brokerage, java.lang.String name, java.lang.String pswd) {
+        this.brokerage = brokerage;
+        this.name = name;
+        this.pswd = pswd;
+    }
+
+    //Returns the screen name for this trader.
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    //Returns the password for this trader.
+
+    public java.lang.String getPassword() {
+        return pswd;
+    }
+
+    //Indicates whether this trader is equal to the other, based on comparing their screen names case blind
+
+    public boolean equals(Object other) {
+        return (this.compareTo((Trader) other) == 0);
+    }
+
+    //Creates a new TraderWindow for this trader and saves a reference to it in myWindow
+
+    public void openWindow() {
+        TraderWindow myWindow = new TraderWindow(this);
+        //finish when mailbox class done
+    }
+
+    //Returns true if this trader has any messages in its mailbox.
+
+    public boolean hasMessages() {
+        //finish when mailbox class done
+        return true;
+    }
+
+    /**
+     * Adds msg to this trader's mailbox and displays all messages. If this trader is logged in 
+     * (myWindow is not null) removes and shows all the messages in the mailbox by calling myWindow.showMessage(msg) 
+     * for each msg in the mailbox.
+     * @param msg
+     */
+
+    public void receiveMessage(String msg) {
+        //finish when mailbox class done 
+    }
+    
+    public void getQuote(String symbol) {
+        //finish when brokerage class done
+    }
+    
+    public void placeOrder(TradeOrder order) {
+        //finish when tradeorder class done
+    }
+    
+    public void quit() {
+        //finish when brokerage class done 
+    }
+
 }
