6f9f7b39fe27d2094249a335c4479acc4a248849
plaiapaul <paulplaia18@marist.com>
2017-03-23 19:55:15 -0400

    Update Trader.java

--- Trader.java
+++ Trader.java
@@ -1,20 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Trader implements Comparable<Trader> {
     private Brokerage brokerage;
     private String name;
     private String pswd;
+    private Queue<String> mailbox;
+    private TraderWindow myWindow;
 
-    @Override
-    public int compareTo(Trader other) {
-        return this.getName().toLowerCase().compareTo(other.getName().toLowerCase());
-    }
-
-    //Constructs a new trader, affiliated with a given brockerage, with a given screen name and password.
+    //Constructs a new trader, affiliated with a given brokerage, with a given screen name and password.
 
     public Trader(Brokerage brokerage, java.lang.String name, java.lang.String pswd) {
         this.brokerage = brokerage;
         this.name = name;
         this.pswd = pswd;
+        this.mailbox = new LinkedList<String>();
     }
 
     //Returns the screen name for this trader.
@@ -31,45 +31,53 @@ public class Trader implements Comparable<Trader> {
 
     //Indicates whether this trader is equal to the other, based on comparing their screen names case blind
 
-    public boolean equals(Object other) {
-        return (this.compareTo((Trader) other) == 0);
+    public boolean equals(Object obj) {
+        if (obj instanceof Trader) return this.compareTo((Trader) obj) == 0;
+        throw new ClassCastException();
     }
+
+    public int compareTo(Trader other) {
+        return this.getName().toLowerCase().compareTo(other.getName().toLowerCase());
+    }
+
 
     //Creates a new TraderWindow for this trader and saves a reference to it in myWindow
 
     public void openWindow() {
-        TraderWindow myWindow = new TraderWindow(this);
-        //finish when mailbox class done
+        myWindow = new TraderWindow(this);
+        while (hasMessages()) myWindow.showMessage(mailbox.poll());
     }
 
     //Returns true if this trader has any messages in its mailbox.
 
     public boolean hasMessages() {
-        //finish when mailbox class done
-        return true;
+        return !mailbox.isEmpty();
     }
 
     /**
-     * Adds msg to this trader's mailbox and displays all messages. If this trader is logged in 
-     * (myWindow is not null) removes and shows all the messages in the mailbox by calling myWindow.showMessage(msg) 
+     * Adds msg to this trader's mailbox and displays all messages. If this trader is logged in
+     * (myWindow is not null) removes and shows all the messages in the mailbox by calling myWindow.showMessage(msg)
      * for each msg in the mailbox.
+     *
      * @param msg
      */
 
     public void receiveMessage(String msg) {
-        //finish when mailbox class done 
+        mailbox.add(msg);
+        while (hasMessages()) myWindow.showMessage(mailbox.poll());
     }
-    
+
     public void getQuote(String symbol) {
-        //finish when brokerage class done
+        brokerage.getQuote(symbol, this);
     }
-    
+
     public void placeOrder(TradeOrder order) {
-        //finish when tradeorder class done
+        brokerage.placeOrder(order);
     }
-    
+
     public void quit() {
-        //finish when brokerage class done 
+        brokerage.logout(this);
+        myWindow = null;
     }
 
 }
